d078d7c4b2b18b15a8f6e1b680d5cd3c8cabe574
leogracas34-gif <leogracas34@gmail.com>
2026-04-15 06:49:10 -0300

    Refactor VLTVApplication to remove WorkManager setup
    Removed WorkManager configuration and related injection.

--- app/src/main/java/com/vltvplus/VLTVApplication.kt
+++ app/src/main/java/com/vltvplus/VLTVApplication.kt
@@ -1,21 +1,10 @@
 package com.vltvplus
 
 import android.app.Application
-import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
 
 @HiltAndroidApp
-class VLTVApplication : Application(), Configuration.Provider {
-
-    @Inject
-    lateinit var workerFactory: androidx.hilt.work.HiltWorkerFactory
-
-    // Correção: Alterado de 'fun getWorkManagerConfiguration()' para 'val workManagerConfiguration'
-    override val workManagerConfiguration: Configuration
-        get() = Configuration.Builder()
-            .setWorkerFactory(workerFactory)
-            .build()
+class VLTVApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
@@ -27,3 +16,4 @@ class VLTVApplication : Application(), Configuration.Provider {
             private set
     }
 }
+
